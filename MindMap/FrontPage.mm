07695272b4f2617551d823e9d56d33fde4f28449
Tsuyoshi Iguchi <tsuyoshi.iguchi@gmail.com>
2021-06-05 02:51:08 +0900

    streaming 2021/06/05 (2).

--- MindMap/FrontPage.mm
+++ MindMap/FrontPage.mm
@@ -1,4 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1622824006681" ID="ID_1258559428" MODIFIED="1622824023823" TEXT="FrontPage"/>
+<node CREATED="1622824006681" ID="ID_1258559428" MODIFIED="1622824023823" TEXT="FrontPage">
+<node CREATED="1622826976797" ID="ID_512052732" MODIFIED="1622826980832" POSITION="right" TEXT="&#x65e5;&#x672c;&#x8a9e;"/>
+<node CREATED="1622827179900" ID="ID_1217932830" MODIFIED="1622827182305" POSITION="right" TEXT="&#x3042;&#x3044;&#x3046;&#x3048;&#x304a;"/>
+</node>
 </map>
